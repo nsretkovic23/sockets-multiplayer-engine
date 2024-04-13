103123b337eefbdd8e77ecac7ee2ab4087416bf7
Nikola Sretkovic <nsretkovic23@gmail.com>
2024-04-13 14:13:45 +0200

    Create samples for client and server that use server engine
    - Server uses server-engine for creating lobbies
    - Client just connects to server for now

---
+++ client/go.mod
@@ -0,0 +1,3 @@
+module wordguess_client
+
+go 1.22.2
---
+++ server/samples/word_guess_server.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"socket_server/engine"
+	"socket_server/helpers"
+)
+
+// TODO: These should be in a config file, environment variables or command line arguments
+const (
+	SMILEY      = "\U0001F604"
+	MAX_CONN    = 2
+	MAX_LOBBIES = 1
+	PORT        = 8080
+	SECRET_WORD = "MY WORD"
+)
+
+type Message struct {
+	Text string
+}
+
+func main() {
+	helpers.PrintInfo("Starting server...")
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", PORT))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	helpers.PrintInfo(fmt.Sprintf("Server started, listening on port %d", PORT))
+
+	var lobbies []engine.Lobby
+	for len(lobbies) < MAX_LOBBIES {
+
+		lobby, err := engine.MakeLobby(listener, MAX_CONN, len(lobbies),
+			engine.FormatMessage[*Message](&Message{"Welcome to the game!"}))
+		if err != nil {
+			fmt.Println("Error while trying to make a lobby", err)
+			continue
+		}
+
+		fmt.Println("Lobby created", lobby.Id)
+
+		lobbies = append(lobbies, *lobby)
+		go runGame(lobby)
+	}
+
+	for len(lobbies) > 0 {
+
+	}
+}
+
+func runGame(lobby *engine.Lobby) {
+	fmt.Println("Running game")
+	if lobby.Conns == nil || len(lobby.Conns) <= 1 {
+		helpers.PrintRed("Not enough players in the lobby")
+		return
+	}
+
+	for i, conn := range lobby.Conns {
+		_, err := conn.Write(engine.FormatMessage(&Message{fmt.Sprintf("You are player %d\n", i)}))
+		if err != nil {
+			fmt.Println("Error sending message to player", err)
+		}
+	}
+
+	for len(lobby.Conns) > 0 {
+		// playerMsg := make([]byte, 1024)
+		// _, err := lobby.conns[0].Read(playerMsg)
+		// if err != nil {
+		// 	fmt.Println("Error reading player message", err)
+		// }
+	}
+}
