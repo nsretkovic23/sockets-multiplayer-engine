2f72531eb70ef76991a67d488c5bb0f25145a902
Nikola Sretkovic <nsretkovic23@gmail.com>
2024-04-23 00:26:55 +0200

    Refactor, move code parts to separate functions

--- samples/word_guess_game/server/server.go
+++ samples/word_guess_game/server/server.go
@@ -14,7 +14,7 @@ import (
 const (
 	SMILEY      = "\U0001F604"
 	MAX_CONN    = 2
-	MAX_LOBBIES = 1
+	MAX_LOBBIES = 2
 	PORT        = 8080
 	SECRET_WORD = "MY WORD"
 	MIN_CONN    = 2
@@ -28,6 +28,8 @@ type ServerMessage struct {
 	Turn            int
 	PreviousGuesses []string
 	GuessState      string
+	WinnerTag       int
+	WonByDisconnect bool
 }
 
 type ClientMessage struct {
@@ -59,21 +61,19 @@ func main() {
 	var lobbies []engine.Lobby
 	for len(lobbies) < MAX_LOBBIES {
 
-		lobby, err := engine.MakeLobby(listener, MAX_CONN, len(lobbies),
-			engine.FormatMessage[*ServerMessage](&ServerMessage{
-				"welcome",
-				"Welcome to the game!",
-				-1,
-				-1,
-				[]string{},
-				"",
-			}))
+		lobby, err := engine.MakeLobby(
+			listener,
+			MAX_CONN,
+			len(lobbies),
+			engine.FormatMessage(&ServerMessage{"welcome", "Welcome to the game!", -1, -1, []string{}, "", -1, false}),
+		)
+
 		if err != nil {
 			fmt.Println("Error while trying to make a lobby", err)
 			continue
 		}
 
-		fmt.Println("Lobby created", lobby.Id)
+		fmt.Println("Lobby created with Id:", lobby.Id)
 
 		lobbies = append(lobbies, *lobby)
 		go runGame(lobby)
@@ -85,38 +85,10 @@ func main() {
 }
 
 func runGame(lobby *engine.Lobby) {
-	fmt.Println("Running game")
-	if lobby.Conns == nil || len(lobby.Conns) <= 1 {
-		helpers.PrintRed("Not enough players in the lobby")
-		return
-	}
+	fmt.Println(lobbyString(lobby), ": Running game")
 
-	// TODO: Refactor
-	state := &GameState{
-		0,
-		[]string{},
-		word.InitializeLetterMap(),
-		"NIKOLA SRETKOVIC",
-		false,
-		"",
-	}
-
-	// TODO: Refactor
-	// WELCOME MESSAGE
-	for i, conn := range lobby.Conns {
-		msg := &ServerMessage{
-			"tag_assignment",
-			fmt.Sprintf("You are player %d", i),
-			i,
-			0,
-			[]string{},
-			word.FormatSentenceGuessState(state.Word, state.GuessedLetters),
-		}
-		_, err := engine.SendUnicastMessage(&conn, engine.FormatMessage[*ServerMessage](msg))
-		if err != nil {
-			fmt.Println("Error sending message to player", err)
-		}
-	}
+	state := createInitialState()
+	sendTagAssignments(lobby, state)
 
 	// TODO: Refactor
 	// GAME LOOP
@@ -124,30 +96,30 @@ func runGame(lobby *engine.Lobby) {
 		// Send message to all players about the next turn
 		msg := state.getNextTurnMessage()
 		engine.SendMulticastMessage(&lobby.Conns, msg)
+		// Listen for guess from the player whose turn it is
+		msgRaw := make([]byte, 4096)
+		n, err := state.listenForNextTurn(lobby, &msgRaw)
 
-		msgRaw := make([]byte, 2048)
-		lobby.Conns[state.Turn].SetReadDeadline(time.Now().Add(TIMEOUT * time.Second))
-		n, err := lobby.Conns[state.Turn].Read(msgRaw)
 		if err != nil {
-
-			if engine.IsTimeoutError(err) {
-				helpers.PrintRed("Client input timeout, SEND MESSAGE TO PLAYERS and continue")
+			if isGameOverOnError(lobby, state, err) {
+				break
 			} else {
-				// TODO: See if you can handle the error better here, this else is pretty general and maybe doesn't always mean that the player disconnected
-
-				helpers.PrintRed("Player " + /*fmt.Sprintf("%d", state.Turn)*/ lobby.Conns[state.Turn].RemoteAddr().String() + " disconnected")
-				lobby.Conns = append(lobby.Conns[:state.Turn], lobby.Conns[state.Turn+1:]...)
-
-				if len(lobby.Conns) < MIN_CONN {
-					helpers.PrintRed("Not enough players in the lobby, game over!")
-					helpers.PrintRed("If there is a player in the lobby, message him that he won!")
-					break
-				}
-
+				state.Turn = (state.Turn + 1) % len(lobby.Conns)
+				continue
 			}
+			// if engine.IsTimeoutError(err) {
+			// 	helpers.PrintRed(lobbyString(lobby) + ": Client input timeout, SEND MESSAGE TO PLAYERS and continue")
+			// } else {
+			// 	helpers.PrintRed(lobbyString(lobby) + ": Player " + lobby.Conns[state.Turn].RemoteAddr().String() + " disconnected")
+			// 	engine.RemoveConn(&lobby.Conns[state.Turn], &lobby.Conns)
 
-			state.Turn = (state.Turn + 1) % len(lobby.Conns)
-			continue
+			// 	if len(lobby.Conns) < MIN_CONN {
+			// 		finishGameOnNotEnoughPlayers(lobby, state)
+			// 		break
+			// 	}
+			// }
+			// state.Turn = (state.Turn + 1) % len(lobby.Conns)
+			// continue
 		}
 
 		var clientMsg ClientMessage
@@ -159,25 +131,93 @@ func runGame(lobby *engine.Lobby) {
 		}
 
 		if clientMsg.Sender != state.Turn {
-			helpers.PrintRed("Player " + fmt.Sprintf("%d", state.Turn) + " tried to guess out of turn")
+			helpers.PrintRed(lobbyString(lobby) + "Player " + fmt.Sprintf("%d", state.Turn) + " tried to guess out of turn")
 			continue
 		}
 
 		word.ProcessInput(state.Word, clientMsg.Guess, state.GuessedLetters, &state.IsWordGuessed, &state.Message)
 
 		if state.IsWordGuessed {
-			helpers.PrintInfo("Lobby " + fmt.Sprint(lobby.Id) + "::: Player " + fmt.Sprintf("%d", state.Turn) + " guessed the word/sentence correctly, game over!")
+			helpers.PrintGreen(lobbyString(lobby) + ": Player " + fmt.Sprintf("%d", state.Turn) + " guessed the word/sentence correctly, game over!")
 			// Since turn is not yet incremented, the player who guessed the word is the current player, so we can send state.Turn
-			msg := engine.FormatMessage(&ServerMessage{"game_over", state.Message, -1, state.Turn, state.PreviousGuesses, state.Word})
+			msg := engine.FormatMessage(&ServerMessage{"game_over", state.Message, -1, state.Turn, state.PreviousGuesses, state.Word, state.Turn, false})
 			engine.SendMulticastMessage(&lobby.Conns, msg)
 			break
 		}
 
 		state.PreviousGuesses = append(state.PreviousGuesses, clientMsg.Guess)
-		helpers.PrintInfo("Lobby " + fmt.Sprint(lobby.Id) + "::: Player " + fmt.Sprintf("%d", state.Turn) + " guessed: " + clientMsg.Guess)
+		helpers.PrintInfo(lobbyString(lobby) + ": Player " + fmt.Sprintf("%d", state.Turn) + " guessed: " + clientMsg.Guess)
 		state.Turn = (state.Turn + 1) % len(lobby.Conns)
 	}
 
+}
+
+func isGameOverOnError(lobby *engine.Lobby, state *GameState, err error) bool {
+	if engine.IsTimeoutError(err) {
+		helpers.PrintRed(lobbyString(lobby) + ": Client input timeout, SEND MESSAGE TO PLAYERS and continue")
+		return false
+	} else {
+		helpers.PrintRed(lobbyString(lobby) + ": Player " + lobby.Conns[state.Turn].RemoteAddr().String() + " disconnected")
+		engine.RemoveConn(&lobby.Conns[state.Turn], &lobby.Conns)
+
+		if len(lobby.Conns) < MIN_CONN {
+			finishGameOnNotEnoughPlayers(lobby, state)
+			return true
+		}
+	}
+
+	return false
+}
+
+func (state *GameState) listenForNextTurn(lobby *engine.Lobby, msgRaw *[]byte) (int, error) {
+	lobby.Conns[state.Turn].SetReadDeadline(time.Now().Add(TIMEOUT * time.Second))
+	return lobby.Conns[state.Turn].Read(*msgRaw)
+}
+
+func createInitialState() *GameState {
+	return &GameState{
+		0,
+		[]string{},
+		word.InitializeLetterMap(),
+		"NIKOLA SRETKOVIC",
+		false,
+		"",
+	}
+}
+
+func sendTagAssignments(lobby *engine.Lobby, state *GameState) {
+	for i, conn := range lobby.Conns {
+		msg := &ServerMessage{
+			"tag_assignment",
+			fmt.Sprintf("You are player %d", i),
+			i,
+			0,
+			[]string{},
+			word.FormatSentenceGuessState(state.Word, state.GuessedLetters),
+			-1,
+			false,
+		}
+		_, err := engine.SendUnicastMessage(&conn, engine.FormatMessage[*ServerMessage](msg))
+		if err != nil {
+			fmt.Println("Error sending message to player", err)
+		}
+	}
+}
+
+func finishGameOnNotEnoughPlayers(lobby *engine.Lobby, state *GameState) {
+	helpers.PrintRed(fmt.Sprintf("Lobby %d : Not enough players in the lobby, game over", lobby.Id))
+	msg := engine.FormatMessage(&ServerMessage{
+		"game_over",
+		"Not enough players in the lobby to continue the game. You Won!",
+		-1,
+		state.Turn,
+		state.PreviousGuesses,
+		state.Word,
+		-1,
+		true,
+	})
+
+	engine.SendMulticastMessage(&lobby.Conns, msg)
 }
 
 func (state *GameState) getNextTurnMessage() []byte {
@@ -188,5 +228,11 @@ func (state *GameState) getNextTurnMessage() []byte {
 		state.Turn,
 		state.PreviousGuesses,
 		word.FormatSentenceGuessState(state.Word, state.GuessedLetters),
+		-1,
+		false,
 	})
+}
+
+func lobbyString(lobby *engine.Lobby) string {
+	return fmt.Sprintf("Lobby %d", lobby.Id)
 }
